9a1e73df8c77880631e45b3acb3bccc804c0a7c6
Sheldon Odhiambo <sheldonodhiuma04@gmail.com>
2026-01-22 13:36:51 +0300

    Add debug logs

--- index.tsx
+++ index.tsx
@@ -1,16 +1,39 @@
 
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+
+// const rootElement = document.getElementById('root');
+// if (!rootElement) {
+//   throw new Error("Could not find root element to mount to");
+// }
+
+// const root = ReactDOM.createRoot(rootElement);
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+console.log("🟢 index.tsx loaded");
+
 const rootElement = document.getElementById('root');
+console.log("🟢 root element:", rootElement);
+
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error("❌ Could not find root element to mount to");
 }
 
 const root = ReactDOM.createRoot(rootElement);
+console.log("🟢 React root created");
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+console.log("🟢 App render called");
